d823f677c9bc60c01f123a108cab11d329f7e4d3
Dunaeva <evadunaeva04@gmail.com>
2026-02-21 18:18:23 +0300

    class Message

--- WebApplication2/Program.cs
+++ WebApplication2/Program.cs
@@ -67,3 +67,11 @@ app.MapDelete("/forum", () =>
 });
 
 app.Run();
+
+public class Message
+{
+    public int Id { get; set; }
+    public string Author { get; set; }
+    public string Text { get; set; }
+    
+}
