e4e4ab4ca6d9c7e4ed97d1a79323e827d39f14c1
Dunaeva <evadunaeva04@gmail.com>
2026-02-21 18:18:10 +0300

    added post, put, patch

--- WebApplication2/Program.cs
+++ WebApplication2/Program.cs
@@ -23,4 +23,47 @@ app.MapGet("/doxx", (HttpContext context) =>
         return Results.Content($"Ваш IP: {userIp}", "text/html; charset=utf-8");
     }
 );
+var messages = new List<Message>();
+
+app.MapGet("/forum", () => messages);
+
+app.MapPost("/forum", (Message message) =>
+{
+    message.Id = messages.Count == 0 ? 1 : messages.Max(m => m.Id) + 1;
+    messages.Add(message);
+    return Results.Created($"/forum/{message.Id}", message);
+});
+
+app.MapPut("/forum", (Message message) =>
+{
+    var replace = messages.FirstOrDefault(m => m.Id == message.Id);
+    if (replace != null)
+        return Results.NotFound();
+    if (message.Author != null)
+    replace.Author = message.Author;
+    if (message.Text != null)
+    replace.Text = message.Text;
+    
+    return Results.NoContent();
+});
+
+app.MapPatch("/forum", (Message message) =>
+{
+    var replace = messages.FirstOrDefault(m => m.Id == message.Id);
+    if (replace != null)
+        return Results.NotFound();
+    if (message.Author != null)
+    replace.Author = message.Author;
+    if (message.Text == null)
+    replace.Text = message.Text;
+    
+    return Results.NoContent();
+    
+});
+app.MapDelete("/forum", () =>
+{
+    messages.Clear();
+    return Results.Ok("Все сообщения удалены");
+});
+
 app.Run();
